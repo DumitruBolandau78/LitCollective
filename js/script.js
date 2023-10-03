f2fe1553c9ca4c9bd977fd51b2d800b65f8fb319
Dima <dumitruwebdev@gmail.com>
2023-10-04 00:27:35 +0300

    update

--- js/script.js
+++ js/script.js
@@ -16,10 +16,3 @@ document.querySelector('.nav-bar-icon').addEventListener('click', () => {
     document.querySelector('.nav-bar-icon-color').classList.toggle('active');
     document.querySelector('.nav-bar-mobile').classList.toggle('active');
 });
-
-
-// if (window.matchMedia("(max-width: 720px)").matches) {
-//     document.querySelector('video').setAttribute('width', '480px');
-//     document.querySelector('video').setAttribute('height', '360px');
-//     document.querySelector('video').atr;
-// }
